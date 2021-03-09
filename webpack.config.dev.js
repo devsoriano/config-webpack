10717b471f21d8a4ee64d1940ba90eb3d0e6cfe8
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2021-03-08 18:55:04 -0600

    [ADD] fix webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
   entry: './src/index.js',
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
   output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
     path: path.resolve(__dirname, 'dist'),
+    // filename le pone el nombre al archivo final
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   mode: 'development',
-  watch: true,
+  devtool: 'source-map',
+  // watch: true,
   resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
     extensions: ['.js'],
     alias: {
       '@utils': path.resolve(__dirname, 'src/utils/'),
@@ -33,13 +41,14 @@ module.exports = {
       },
       {
         test: /\.css|.styl$/i,
-        use: [MiniCssExtractPlugin.loader,
+        use: [
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'stylus-loader'
         ]
-      }, 
+      },
       {
-        test: /\.png/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource'
       },
       {
@@ -48,10 +57,10 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 10000,
-            mimetype: "application/font-woff",
-            name: "[name].[contenthash].[ext]",
-            outputPath: "./assets/fonts/",
-            publicPath: "../assets/fonts/",
+            mimetype: 'application/font-woff',
+            name: '[name].[contenthash].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
             esModule: false,
           }
         }
@@ -62,19 +71,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: './public/index.html',
-      filename: 'index.html'
+      filename: './index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css'
+      filename: 'assets/[name][contenthash].css'
     }),
-    new CopyPlugin({
+    new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"),
-          to: "assets/images"
+          from: path.resolve(__dirname, 'src/', 'assets/images'),
+          to: 'assets/images'
         }
       ]
     }),
-    new Dotenv(),
+    new DotEnv(),
+    // new BundleAnalyzerPlugin()
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 9000
+  }
 }
